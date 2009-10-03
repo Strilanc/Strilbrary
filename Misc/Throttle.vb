a75dfc33993e91f8b9999d70b4b9120ade5cf7e8
Strilanc <Craig.Gidney@gmail.com>
2009-10-03 16:14:21 -0300

    Throttle tests - Rewrote Throttle and moved it to Threading - Fixed a bug where changing system time affected Throttle cooldown - Added unit tests for Throttle

--- Misc/Throttle.vb
+++ Misc/Throttle.vb
@@ -1,47 +1,40 @@
 ﻿Imports System.Threading
 
-Public NotInheritable Class Throttle
-    Private ReadOnly cooldown As TimeSpan
-    Private nextAction As Action
-    Private readyTime As Date
-    Private running As Boolean
-    Private ReadOnly lock As New Object()
+Namespace Threading
+    '''<summary>Runs specified actions while respecting a minimum cooldown.</summary>
+    Public NotInheritable Class Throttle
+        Private ReadOnly cooldown As TimeSpan
+        Private nextAction As Action
+        Private running As Boolean
+        Private ReadOnly ref As ICallQueue = New ThreadPooledCallQueue()
 
-    <ContractInvariantMethod()> Private Sub ObjectInvariant()
-        Contract.Invariant(nextAction IsNot Nothing)
-        Contract.Invariant(cooldown.Ticks >= 0)
-    End Sub
+        Public Sub New(ByVal cooldown As TimeSpan)
+            Me.cooldown = cooldown
+        End Sub
 
-    Public Sub New(ByVal cooldown As TimeSpan)
-        Contract.Requires(cooldown.Ticks >= 0)
-        Me.cooldown = cooldown
-        Me.readyTime = Now
-        Me.nextAction = Sub()
-                        End Sub
-    End Sub
-    Public Sub SetActionToRun(ByVal action As Action)
-        Contract.Requires(action IsNot Nothing)
+        '''<summary>Sets the action to run when the cooldown finishes.</summary>
+        Public Sub SetActionToRun(ByVal action As Action)
+            ref.QueueAction(Sub()
+                                nextAction = action
+                                If Not running Then
+                                    running = True
+                                    RunNextAction()
+                                End If
+                            End Sub)
+        End Sub
 
-        Dim t = Now()
-        SyncLock lock
-            nextAction = action
-            If running Then Return
-            running = True
-        End SyncLock
-
-        If t >= readyTime Then
-            Execute()
-        Else
-            FutureWait(readyTime - t).CallOnSuccess(AddressOf Execute)
-        End If
-    End Sub
-    Private Sub Execute()
-        Dim action As action
-        SyncLock lock
-            action = nextAction
-            readyTime = Now() + cooldown
-            running = False
-        End SyncLock
-        Call action()
-    End Sub
-End Class
+        '''<summary>Runs the next action and resets the cooldown.</summary>
+        Private Sub RunNextAction()
+            ref.QueueAction(Sub()
+                                Dim action = nextAction
+                                nextAction = Nothing
+                                If action IsNot Nothing Then
+                                    Call ThreadPooledAction(action)
+                                    FutureWait(cooldown).CallOnSuccess(AddressOf RunNextAction)
+                                Else
+                                    running = False
+                                End If
+                            End Sub)
+        End Sub
+    End Class
+End Namespace
---
+++ Testing/ThrottleTest.vb
@@ -0,0 +1,48 @@
+﻿Imports System
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Strilbrary.Threading
+
+<TestClass()>
+Public Class ThrottleTest
+    <TestMethod()>
+    Public Sub RunTest()
+        Dim lock = New System.Threading.ManualResetEvent(initialState:=False)
+        Dim throttle = New Throttle(New TimeSpan(0, 0, 0, 0, milliseconds:=1000))
+        throttle.SetActionToRun(Sub()
+                                    lock.Set()
+                                End Sub)
+        Assert.IsTrue(lock.WaitOne(millisecondsTimeout:=50))
+    End Sub
+
+    <TestMethod()>
+    Public Sub HoldTest()
+        Dim lock = New System.Threading.ManualResetEvent(initialState:=False)
+        Dim throttle = New Throttle(New TimeSpan(0, 0, 0, 0, milliseconds:=100))
+
+        throttle.SetActionToRun(Sub()
+                                    lock.Set()
+                                End Sub)
+        lock.WaitOne(millisecondsTimeout:=50)
+
+        lock.Reset()
+        throttle.SetActionToRun(Sub()
+                                    lock.Set()
+                                End Sub)
+        Assert.IsTrue(Not lock.WaitOne(millisecondsTimeout:=50))
+        Assert.IsTrue(lock.WaitOne(millisecondsTimeout:=1000))
+    End Sub
+
+    <TestMethod()>
+    Public Sub ReentrantTest()
+        Dim lock = New System.Threading.ManualResetEvent(initialState:=False)
+        Dim throttle = New Throttle(New TimeSpan(0, 0, 0, 0, milliseconds:=100))
+
+        throttle.SetActionToRun(Sub()
+                                    throttle.SetActionToRun(Sub()
+                                                                lock.Set()
+                                                            End Sub)
+                                End Sub)
+        Assert.IsTrue(Not lock.WaitOne(millisecondsTimeout:=50))
+        Assert.IsTrue(lock.WaitOne(millisecondsTimeout:=1000))
+    End Sub
+End Class
