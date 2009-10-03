4bdbbccf4b25959bd9de1cfc6460f4be60c28182
Strilanc <Craig.Gidney@gmail.com>
2009-10-03 05:59:23 -0300

    LazyValue tests - Renamed ExpensiveValue<T> to LazyValue<T> - LazyValue<T> now nulls its func to allow GC - Added tests for LazyValue<T>

---
+++ Misc/LazyValue.vb
@@ -0,0 +1,33 @@
+﻿'''<summary>Wraps a value which is computed and stored only once needed.</summary>
+Public NotInheritable Class LazyValue(Of T)
+    Private func As Func(Of T)
+    Private _value As T
+
+    Public Sub New(ByVal func As Func(Of T))
+        Contract.Requires(func IsNot Nothing)
+        Me.func = func
+    End Sub
+    Public Sub New(ByVal value As T)
+        Me._value = value
+    End Sub
+
+    Public ReadOnly Property Value As T
+        Get
+            If func IsNot Nothing Then
+                _value = func()
+                func = Nothing
+            End If
+            Return _value
+        End Get
+    End Property
+
+    Public Shared Widening Operator CType(ByVal func As Func(Of T)) As LazyValue(Of T)
+        Contract.Requires(func IsNot Nothing)
+        Contract.Ensures(Contract.Result(Of LazyValue(Of T))() IsNot Nothing)
+        Return New LazyValue(Of T)(func)
+    End Operator
+    Public Shared Widening Operator CType(ByVal value As T) As LazyValue(Of T)
+        Contract.Ensures(Contract.Result(Of LazyValue(Of T))() IsNot Nothing)
+        Return New LazyValue(Of T)(value)
+    End Operator
+End Class
---
+++ Testing/LazyValueTest.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports strilbrary
+
+<TestClass()>
+Public Class LazyValueTest
+    <TestMethod()>
+    Public Sub ValueTest()
+        Assert.IsTrue(New LazyValue(Of Integer)(1).Value = 1)
+        Assert.IsTrue(New LazyValue(Of Integer)(2).Value = 2)
+        Assert.IsTrue(CType(3, LazyValue(Of Integer)).Value = 3)
+        Assert.IsTrue(CType(4, LazyValue(Of Integer)).Value = 4)
+    End Sub
+
+    <TestMethod()>
+    Public Sub FuncTest()
+        Assert.IsTrue(New LazyValue(Of Integer)(Function() 1).Value = 1)
+        Assert.IsTrue(New LazyValue(Of Integer)(Function() 2).Value = 2)
+        Dim f3 As Func(Of Integer) = Function() 3
+        Dim f4 As Func(Of Integer) = Function() 4
+        Assert.IsTrue(CType(f3, LazyValue(Of Integer)).Value = 3)
+        Assert.IsTrue(CType(f4, LazyValue(Of Integer)).Value = 4)
+    End Sub
+
+    <TestMethod()>
+    Public Sub FuncTestIdempotent()
+        Dim state = 0
+        Dim val = New LazyValue(Of Integer)(Function() As Integer
+                                                state += 1
+                                                Return state
+                                            End Function)
+        Assert.IsTrue(state = 0)
+        Assert.IsTrue(val.Value = 1)
+        Assert.IsTrue(state = 1)
+        Assert.IsTrue(val.Value = 1)
+        Assert.IsTrue(state = 1)
+    End Sub
+End Class
