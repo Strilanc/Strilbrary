0be1388f7a951bfadf4a3b3e642e0e226c57fc09
unknown <Craig@.(none)>
2011-02-10 15:52:17 -0400

    Added Strilbrary.Mathematics.Rational - Started Mathematics namespace - Added unit tests

---
+++ Strilbrary/Mathematics/Rational.vb
@@ -0,0 +1,181 @@
+﻿Namespace Mathematics
+    '''<summary>An arbitrary-precision rational number.</summary>
+    <DebuggerDisplay("{ToString()}")>
+    Public Structure Rational
+        Implements IEquatable(Of Rational)
+        Implements IComparable(Of Rational)
+
+        Private ReadOnly _numerator As BigInteger
+        Private ReadOnly _denominator As BigInteger
+
+        Public Shared ReadOnly Zero As Rational = 0
+        Public Shared ReadOnly One As Rational = 1
+
+        <ContractInvariantMethod()> Private Sub ObjectInvariant()
+            Contract.Invariant(_denominator > 0)
+            Contract.Invariant(BigInteger.GreatestCommonDivisor(_numerator, _denominator) = 1)
+        End Sub
+
+        ''' <summary>
+        ''' Creates a Rational equal to numerator/denominator.
+        ''' The assigned numerator/denominator pair may not be the same, because common divisors are cancelled and negatives are moved to the numerator.
+        ''' </summary>
+        Public Sub New(ByVal numerator As BigInteger, ByVal denominator As BigInteger)
+            Contract.Requires(denominator <> 0)
+            Dim gcd = BigInteger.GreatestCommonDivisor(numerator, denominator)
+            If gcd.Sign <> denominator.Sign Then gcd = -gcd
+            Me._numerator = numerator / gcd
+            Me._denominator = denominator / gcd
+            Contract.Assume(_denominator > 0)
+        End Sub
+
+        Public ReadOnly Property Numerator As BigInteger
+            Get
+                Return _numerator
+            End Get
+        End Property
+        Public ReadOnly Property Denominator As BigInteger
+            Get
+                Contract.Ensures(Contract.Result(Of BigInteger)() > 0)
+                Dim r = If(_denominator = 0, 1, _denominator)
+                Contract.Assume(r > 0)
+                Return r
+            End Get
+        End Property
+
+        Public Function CompareTo(ByVal other As Rational) As Integer Implements IComparable(Of Rational).CompareTo
+            Return (Me.Numerator * other.Denominator - other.Numerator * Me.Denominator).Sign
+        End Function
+        Public Overloads Function Equals(ByVal other As Rational) As Boolean Implements IEquatable(Of Rational).Equals
+            Return Me.Numerator = other.Numerator AndAlso Me.Denominator = other.Denominator
+        End Function
+        Public Overrides Function Equals(ByVal obj As Object) As Boolean
+            Return TypeOf obj Is Rational AndAlso Me.Equals(DirectCast(obj, Rational))
+        End Function
+        Public Overrides Function GetHashCode() As Integer
+            Return Me.Numerator.GetHashCode() Xor Me.Denominator.GetHashCode()
+        End Function
+        Public Overrides Function ToString() As String
+            If Denominator = 1 Then Return Numerator.ToString()
+            Return Numerator.ToString() + "/" + Denominator.ToString()
+        End Function
+
+        Public Shared Operator +(ByVal value1 As Rational, ByVal value2 As Rational) As Rational
+            Contract.Assume(value1.Denominator * value2.Denominator <> 0)
+            Return New Rational(value1.Numerator * value2.Denominator + value2.Numerator * value1.Denominator,
+                                value1.Denominator * value2.Denominator)
+        End Operator
+        Public Shared Operator *(ByVal value1 As Rational, ByVal value2 As Rational) As Rational
+            Contract.Assume(value1.Denominator * value2.Denominator <> 0)
+            Return New Rational(value1.Numerator * value2.Numerator,
+                                value1.Denominator * value2.Denominator)
+        End Operator
+        Public Shared Operator /(ByVal value1 As Rational, ByVal value2 As Rational) As Rational
+            Contract.Requires(value2 <> 0)
+            Contract.Assume(value1.Denominator * value2.Numerator <> 0)
+            Return New Rational(value1.Numerator * value2.Denominator,
+                                value1.Denominator * value2.Numerator)
+        End Operator
+        Public Shared Operator -(ByVal value As Rational) As Rational
+            Contract.Assume(value.Denominator <> 0)
+            Return New Rational(-value.Numerator, value.Denominator)
+        End Operator
+        Public Shared Operator Mod(ByVal value1 As Rational, ByVal value2 As Rational) As Rational
+            Contract.Assume(value1.Denominator * value2.Denominator <> 0)
+            Return New Rational((value1.Numerator * value2.Denominator) Mod (value2.Numerator * value1.Denominator),
+                                value1.Denominator * value2.Denominator)
+        End Operator
+        Public Shared Operator -(ByVal value1 As Rational, ByVal value2 As Rational) As Rational
+            Return value1 + -value2
+        End Operator
+
+        Public Shared Operator =(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return value1.Equals(value2)
+        End Operator
+        Public Shared Operator <>(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return Not value1.Equals(value2)
+        End Operator
+        Public Shared Operator >(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return value1.CompareTo(value2) > 0
+        End Operator
+        Public Shared Operator >=(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return value1.CompareTo(value2) >= 0
+        End Operator
+        Public Shared Operator <=(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return value1.CompareTo(value2) <= 0
+        End Operator
+        Public Shared Operator <(ByVal value1 As Rational, ByVal value2 As Rational) As Boolean
+            Return value1.CompareTo(value2) < 0
+        End Operator
+
+        Public Shared Widening Operator CType(ByVal value As Byte) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As SByte) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As Int16) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As UInt16) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As Int32) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As UInt32) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As Int64) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As UInt64) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+        Public Shared Widening Operator CType(ByVal value As BigInteger) As Rational
+            Contract.Assume(CType(1, BigInteger) <> 0)
+            Return New Rational(value, 1)
+        End Operator
+    End Structure
+    Public Module RationalExtensions
+        '''<summary>Determines the integral result of rounding the rational towards 0.</summary>
+        <Pure()> <Extension()>
+        Public Function Truncate(ByVal value As Rational) As BigInteger
+            Return value.Numerator / value.Denominator 'BigInteger division truncates
+        End Function
+        '''<summary>Determines the integral result of rounding the rational towards negative infinity.</summary>
+        <Pure()> <Extension()>
+        Public Function Floor(ByVal value As Rational) As BigInteger
+            Dim r = value.Numerator / value.Denominator
+            If r > value Then r -= 1
+            Return r
+        End Function
+        '''<summary>Determines the integral result of rounding the rational towards positive infinity.</summary>
+        <Pure()> <Extension()>
+        Public Function Ceiling(ByVal value As Rational) As BigInteger
+            Dim r = value.Numerator / value.Denominator
+            If r < value Then r += 1
+            Return r
+        End Function
+
+        '''<summary>Gets a number that indicates the sign (negative, positive, or zero) of the rational.</summary>
+        <Pure()> <Extension()>
+        Public Function Sign(ByVal value As Rational) As Int32
+            Return value.Numerator.Sign()
+        End Function
+        '''<summary>Returns the absolute value of the rational.</summary>
+        <Pure()> <Extension()>
+        Public Function Abs(ByVal value As Rational) As Rational
+            Contract.Ensures(Contract.Result(Of Rational)() >= 0)
+            Return If(value.Sign() < 0, -value, value)
+        End Function
+    End Module
+End Namespace
---
+++ Testing/RationalTest.vb
@@ -0,0 +1,245 @@
+﻿Imports System
+Imports System.Numerics
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Strilbrary.Mathematics
+
+<TestClass()>
+Public Class RationalTest
+    <TestMethod()>
+    Public Sub RationalConstructorTest()
+        Dim d = New Rational()
+        Dim n0d3 = New Rational(0, 3)
+        Dim n1d1 = New Rational(1, 1)
+        Dim n2d3 = New Rational(2, 3)
+        Dim n3d2 = New Rational(3, 2)
+        Dim n15d3 = New Rational(15, 3)
+        Dim n4d32 = New Rational(4, 32)
+        Dim n1d_1 = New Rational(1, -1)
+        Dim n_1d1 = New Rational(-1, 1)
+        Dim n_1d_1 = New Rational(-1, -1)
+        Dim n6d_6 = New Rational(6, -6)
+        Dim n_10d5 = New Rational(-10, 5)
+
+        Assert.IsTrue(d.Numerator = 0 And d.Denominator = 1)
+        Assert.IsTrue(n0d3.Numerator = 0 And n0d3.Denominator = 1)
+        Assert.IsTrue(n1d1.Numerator = 1 And n1d1.Denominator = 1)
+        Assert.IsTrue(n2d3.Numerator = 2 And n2d3.Denominator = 3)
+        Assert.IsTrue(n3d2.Numerator = 3 And n3d2.Denominator = 2)
+        Assert.IsTrue(n15d3.Numerator = 5 And n15d3.Denominator = 1)
+        Assert.IsTrue(n4d32.Numerator = 1 And n4d32.Denominator = 8)
+        Assert.IsTrue(n1d_1.Numerator = -1 And n1d_1.Denominator = 1)
+        Assert.IsTrue(n_1d1.Numerator = -1 And n_1d1.Denominator = 1)
+        Assert.IsTrue(n_1d_1.Numerator = 1 And n_1d_1.Denominator = 1)
+        Assert.IsTrue(n6d_6.Numerator = -1 And n6d_6.Denominator = 1)
+        Assert.IsTrue(n_10d5.Numerator = -2 And n_10d5.Denominator = 1)
+    End Sub
+
+    <TestMethod()>
+    Public Sub CompareToTest()
+        Assert.IsTrue(New Rational(1, 1).CompareTo(New Rational(0, 1)) > 0)
+        Assert.IsTrue(New Rational(0, 1).CompareTo(New Rational(0, 1)) = 0)
+        Assert.IsTrue(New Rational(-1, 1).CompareTo(New Rational(0, 1)) < 0)
+        Assert.IsTrue(New Rational(3, 5).CompareTo(New Rational(5, 3)) < 0)
+        Assert.IsTrue(New Rational(11, 17).CompareTo(New Rational(1, 2)) > 0)
+        Assert.IsTrue(New Rational(-501, 17).CompareTo(New Rational(1, 2)) < 0)
+        Assert.IsTrue(New Rational(5, -10).CompareTo(New Rational(-1, 2)) = 0)
+
+        Assert.IsTrue(New Rational(1, 1) > New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(0, 1) > New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(-1, 1) > New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(3, 5) > New Rational(5, 3))
+        Assert.IsTrue(New Rational(11, 17) > New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(-501, 17) > New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(5, -10) > New Rational(-1, 2))
+
+        Assert.IsTrue(New Rational(1, 1) >= New Rational(0, 1))
+        Assert.IsTrue(New Rational(0, 1) >= New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(-1, 1) >= New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(3, 5) >= New Rational(5, 3))
+        Assert.IsTrue(New Rational(11, 17) >= New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(-501, 17) >= New Rational(1, 2))
+        Assert.IsTrue(New Rational(5, -10) >= New Rational(-1, 2))
+
+        Assert.IsTrue(Not New Rational(1, 1) < New Rational(0, 1))
+        Assert.IsTrue(Not New Rational(0, 1) < New Rational(0, 1))
+        Assert.IsTrue(New Rational(-1, 1) < New Rational(0, 1))
+        Assert.IsTrue(New Rational(3, 5) < New Rational(5, 3))
+        Assert.IsTrue(Not New Rational(11, 17) < New Rational(1, 2))
+        Assert.IsTrue(New Rational(-501, 17) < New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(5, -10) < New Rational(-1, 2))
+
+        Assert.IsTrue(Not New Rational(1, 1) <= New Rational(0, 1))
+        Assert.IsTrue(New Rational(0, 1) <= New Rational(0, 1))
+        Assert.IsTrue(New Rational(-1, 1) <= New Rational(0, 1))
+        Assert.IsTrue(New Rational(3, 5) <= New Rational(5, 3))
+        Assert.IsTrue(Not New Rational(11, 17) <= New Rational(1, 2))
+        Assert.IsTrue(New Rational(-501, 17) <= New Rational(1, 2))
+        Assert.IsTrue(New Rational(5, -10) <= New Rational(-1, 2))
+    End Sub
+
+    <TestMethod()>
+    Public Sub EqualsTest()
+        Assert.IsTrue(Rational.Zero.Equals(New Rational(0, 1)))
+        Assert.IsTrue(Not Rational.Zero.Equals(New Rational(-1, 1)))
+        Assert.IsTrue(Rational.One.Equals(New Rational(1, 1)))
+        Assert.IsTrue(Rational.One.Equals(New Rational(2, 2)))
+        Assert.IsTrue(Not Rational.One.Equals(New Rational(1, 2)))
+        Assert.IsTrue(New Rational(1, 2).Equals(New Rational(1, 2)))
+        Assert.IsTrue(Not New Rational(1, 2).Equals(New Rational(3, 2)))
+        Assert.IsTrue(Not New Rational(-1, 2).Equals(New Rational(1, 2)))
+        Assert.IsTrue(New Rational(0, 2).Equals(New Rational(0, -3)))
+        Assert.IsTrue(New Rational(0, 2).Equals(New Rational(0, 3)))
+
+        Assert.IsTrue(Rational.Zero = New Rational(0, 1))
+        Assert.IsTrue(Not Rational.Zero = New Rational(-1, 1))
+        Assert.IsTrue(Rational.One = New Rational(1, 1))
+        Assert.IsTrue(Rational.One = New Rational(2, 2))
+        Assert.IsTrue(Not Rational.One = New Rational(1, 2))
+        Assert.IsTrue(New Rational(1, 2) = New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(1, 2) = New Rational(3, 2))
+        Assert.IsTrue(Not New Rational(-1, 2) = New Rational(1, 2))
+        Assert.IsTrue(New Rational(0, 2) = New Rational(0, -3))
+        Assert.IsTrue(New Rational(0, 2) = New Rational(0, 3))
+
+        Assert.IsTrue(Not Rational.Zero <> New Rational(0, 1))
+        Assert.IsTrue(Rational.Zero <> New Rational(-1, 1))
+        Assert.IsTrue(Not Rational.One <> New Rational(1, 1))
+        Assert.IsTrue(Not Rational.One <> New Rational(2, 2))
+        Assert.IsTrue(Rational.One <> New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(1, 2) <> New Rational(1, 2))
+        Assert.IsTrue(New Rational(1, 2) <> New Rational(3, 2))
+        Assert.IsTrue(New Rational(-1, 2) <> New Rational(1, 2))
+        Assert.IsTrue(Not New Rational(0, 2) <> New Rational(0, -3))
+        Assert.IsTrue(Not New Rational(0, 2) <> New Rational(0, 3))
+
+        Assert.IsTrue(Not Rational.One.Equals(New Object()))
+        Assert.IsTrue(Rational.One.Equals(DirectCast(Rational.One, Object)))
+        Assert.IsTrue(Not Rational.One.Equals(DirectCast(Rational.Zero, Object)))
+    End Sub
+
+    <TestMethod()>
+    Public Sub GetHashCodeTest()
+        Assert.IsTrue(Rational.One.GetHashCode() = New Rational(5, 5).GetHashCode())
+    End Sub
+
+    <TestMethod()>
+    Public Sub ToStringTest()
+        Assert.IsTrue(Rational.Zero.ToString() = "0")
+        Assert.IsTrue(Rational.One.ToString() = "1")
+        Assert.IsTrue(New Rational(1, 2).ToString() = "1/2")
+        Assert.IsTrue(New Rational(-1, 2).ToString() = "-1/2")
+        Assert.IsTrue(New Rational(-1, 1).ToString() = "-1")
+    End Sub
+
+    <TestMethod()>
+    Public Sub op_AdditionTest()
+        Assert.IsTrue(Rational.One + Rational.One = New Rational(2, 1))
+        Assert.IsTrue(Rational.One + Rational.Zero = Rational.One)
+        Assert.IsTrue(Rational.One + New Rational(-1, 1) = Rational.Zero)
+        Assert.IsTrue(New Rational(2, 5) + New Rational(-3, 7) = New Rational(-1, 35))
+    End Sub
+    <TestMethod()>
+    Public Sub op_SubtractionTest()
+        Assert.IsTrue(Rational.One - Rational.One = Rational.Zero)
+        Assert.IsTrue(Rational.One - Rational.Zero = Rational.One)
+        Assert.IsTrue(Rational.One - New Rational(-1, 1) = New Rational(2, 1))
+        Assert.IsTrue(New Rational(2, 5) - New Rational(-3, 7) = New Rational(29, 35))
+        Assert.IsTrue(New Rational(-3, 7) - New Rational(2, 5) = New Rational(-29, 35))
+    End Sub
+
+    <TestMethod()>
+    Public Sub op_DivisionTest()
+        Assert.IsTrue(Rational.One / Rational.One = Rational.One)
+        Assert.IsTrue(Rational.Zero / Rational.One = Rational.Zero)
+        Assert.IsTrue(New Rational(-7, 2) / New Rational(3, 5) = New Rational(-35, 6))
+    End Sub
+    <TestMethod()>
+    Public Sub op_ModulusTest()
+        Assert.IsTrue(5 Mod New Rational(2, 3) = New Rational(1, 3))
+        Assert.IsTrue(1 Mod Rational.One = 0)
+    End Sub
+    <TestMethod()>
+    Public Sub op_MultiplyTest()
+        Assert.IsTrue(5 * Rational.One = 5)
+        Assert.IsTrue(New Rational(2, 3) * New Rational(5, 7) = New Rational(10, 21))
+        Assert.IsTrue(New Rational(-2, 3) * New Rational(5, 7) = New Rational(-10, 21))
+    End Sub
+
+    <TestMethod()>
+    Public Sub op_ImplicitTest()
+        Assert.IsTrue(CSByte(1) = Rational.One)
+        Assert.IsTrue(CByte(1) = Rational.One)
+        Assert.IsTrue(1S = Rational.One)
+        Assert.IsTrue(1US = Rational.One)
+        Assert.IsTrue(1 = Rational.One)
+        Assert.IsTrue(1UI = Rational.One)
+        Assert.IsTrue(1L = Rational.One)
+        Assert.IsTrue(1UL = Rational.One)
+        Assert.IsTrue(BigInteger.One = Rational.One)
+
+        Assert.IsTrue(CSByte(0) = Rational.Zero)
+        Assert.IsTrue(CByte(0) = Rational.Zero)
+        Assert.IsTrue(0S = Rational.Zero)
+        Assert.IsTrue(0US = Rational.Zero)
+        Assert.IsTrue(0 = Rational.Zero)
+        Assert.IsTrue(0UI = Rational.Zero)
+        Assert.IsTrue(0L = Rational.Zero)
+        Assert.IsTrue(0UL = Rational.Zero)
+        Assert.IsTrue(BigInteger.Zero = Rational.Zero)
+    End Sub
+
+    <TestMethod()>
+    Public Sub op_UnaryNegationTest()
+        Assert.IsTrue(-Rational.One = New Rational(-1, 1))
+        Assert.IsTrue(-Rational.Zero = Rational.Zero)
+        Assert.IsTrue(-New Rational(-3, 5) = New Rational(3, 5))
+    End Sub
+
+    <TestMethod()>
+    Public Sub AbsTest()
+        Assert.IsTrue(Rational.Zero.Abs() = 0)
+        Assert.IsTrue(Rational.One.Abs() = 1)
+        Assert.IsTrue((-Rational.One).Abs() = 1)
+        Assert.IsTrue(New Rational(3, 5).Abs() = New Rational(3, 5))
+        Assert.IsTrue(New Rational(-2, 5).Abs() = New Rational(2, 5))
+    End Sub
+    <TestMethod()>
+    Public Sub SignTest()
+        Assert.IsTrue(Rational.Zero.Sign() = 0)
+        Assert.IsTrue(Rational.One.Sign() > 0)
+        Assert.IsTrue((-Rational.One).Sign() < 0)
+        Assert.IsTrue(New Rational(3, 5).Sign() > 0)
+        Assert.IsTrue(New Rational(-2, 5).Sign() < 0)
+    End Sub
+
+    <TestMethod()>
+    Public Sub CeilingTest()
+        Assert.IsTrue(Rational.One.Ceiling() = 1)
+        Assert.IsTrue(Rational.Zero.Ceiling() = 0)
+        Assert.IsTrue(New Rational(-1, 1).Ceiling() = -1)
+        Assert.IsTrue(New Rational(7, 5).Ceiling() = 2)
+        Assert.IsTrue(New Rational(2, 5).Ceiling() = 1)
+        Assert.IsTrue(New Rational(-7, 5).Ceiling() = -1)
+        Assert.IsTrue(New Rational(-2, 5).Ceiling() = 0)
+    End Sub
+    <TestMethod()>
+    Public Sub FloorTest()
+        Assert.IsTrue(Rational.One.Floor() = 1)
+        Assert.IsTrue(Rational.Zero.Floor() = 0)
+        Assert.IsTrue(New Rational(-1, 1).Floor() = -1)
+        Assert.IsTrue(New Rational(7, 5).Floor() = 1)
+        Assert.IsTrue(New Rational(2, 5).Floor() = 0)
+        Assert.IsTrue(New Rational(-7, 5).Floor() = -2)
+        Assert.IsTrue(New Rational(-2, 5).Floor() = -1)
+    End Sub
+    <TestMethod()>
+    Public Sub TruncateTest()
+        Assert.IsTrue(Rational.One.Truncate() = 1)
+        Assert.IsTrue(Rational.Zero.Truncate() = 0)
+        Assert.IsTrue(New Rational(-1, 1).Truncate() = -1)
+        Assert.IsTrue(New Rational(7, 5).Truncate() = 1)
+        Assert.IsTrue(New Rational(2, 5).Truncate() = 0)
+        Assert.IsTrue(New Rational(-7, 5).Truncate() = -1)
+        Assert.IsTrue(New Rational(-2, 5).Truncate() = 0)
+    End Sub
+End Class
