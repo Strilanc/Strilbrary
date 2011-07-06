7f0393e0c62116737c072d8f98a3a2ed73449daa
Strilanc <craig.gidney@gmail.com>
2011-07-06 16:36:30 -0300

    CallQueue is now a SynchronizationContext - No longer inherits from TaskScheduler - Queued actions will now have the queue as a synchronization context

--- Strilbrary/Threading/CallQueue.vb
+++ Strilbrary/Threading/CallQueue.vb
@@ -5,9 +5,9 @@ Imports System.Threading
 Namespace Threading
     '''<summary>Runs queued actions in order within a synchronization context, exposing the results as tasks.</summary>
     Public NotInheritable Class CallQueue
-        Inherits TaskScheduler
+        Inherits SynchronizationContext
 
-        Private ReadOnly _consumerQueue As LockFreeConsumer(Of Task)
+        Private ReadOnly _consumerQueue As LockFreeConsumer(Of Action)
 
         <ContractInvariantMethod()> Private Sub ObjectInvariant()
             Contract.Invariant(_consumerQueue IsNot Nothing)
@@ -15,39 +15,43 @@ Namespace Threading
 
         Public Sub New(context As SynchronizationContext)
             Contract.Requires(context IsNot Nothing)
-            Me._consumerQueue = New LockFreeConsumer(Of Task)(
+            Me._consumerQueue = New LockFreeConsumer(Of Action)(
                 context:=context,
-                consumer:=Sub(task)
-                              Dim executed = TryExecuteTask(task)
-                              Contract.Assume(executed)
+                consumer:=Sub(action)
+                              SynchronizationContext.SetSynchronizationContext(Me)
+                              action()
                           End Sub)
         End Sub
 
-        Protected Overrides Function GetScheduledTasks() As IEnumerable(Of Task)
-            Return _consumerQueue
-        End Function
-        Protected Overrides Sub QueueTask(task As Task)
-            _consumerQueue.EnqueueConsume(task)
+        Public Overrides Sub Post(d As SendOrPostCallback, state As Object)
+            _consumerQueue.EnqueueConsume(Sub() d(state))
         End Sub
-        Protected Overrides Function TryExecuteTaskInline(task As Task, taskWasPreviouslyQueued As Boolean) As Boolean
-            Return False
+        Public Overrides Function CreateCopy() As SynchronizationContext
+            Return Me
         End Function
 
         '''<summary>Enqueues an action to be run and exposes it as a task.</summary>
         Public Function QueueAction(action As Action) As Task
             Contract.Requires(action IsNot Nothing)
             Contract.Ensures(Contract.Result(Of Task)() IsNot Nothing)
-            Dim task = New Task(action)
-            task.Start(Me)
-            Return task
+            Dim r = New TaskCompletionSource(Of NoValue)
+            Post(Sub()
+                     action()
+                     r.SetResult(Nothing)
+                 End Sub, Nothing)
+            Contract.Assume(r.Task IsNot Nothing)
+            Return r.Task
         End Function
         '''<summary>Enqueues a function to be run and exposes it as a task.</summary>
         Public Function QueueFunc(Of TReturn)(func As Func(Of TReturn)) As Task(Of TReturn)
             Contract.Requires(func IsNot Nothing)
             Contract.Ensures(Contract.Result(Of Task(Of TReturn))() IsNot Nothing)
-            Dim task = New Task(Of TReturn)(func)
-            task.Start(Me)
-            Return task
+            Dim r = New TaskCompletionSource(Of TReturn)
+            Post(Sub()
+                     r.SetResult(func())
+                 End Sub, Nothing)
+            Contract.Assume(r.Task IsNot Nothing)
+            Return r.Task
         End Function
     End Class
 End Namespace
